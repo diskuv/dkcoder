c93b968f26b4ab07a21e03b6e50128227ca8495d
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-08-11 09:34:59 -0700

    Add ocamlformat to ocamllsp PATH

--- cmake/scripts/dksdk/cmd/exec.cmake
+++ cmake/scripts/dksdk/cmd/exec.cmake
@@ -7,6 +7,17 @@ function(help)
     [QUIET]
     CMD <PROG> <ARGS>
 
+Sets up the correct PATH and other environment variables so that the following
+<PROG> can be run from the command line, especially in IDEs like Visual Studio Code,
+if they have been built in DkSDK:
+    dune
+    flexlink
+    ocamlc
+    ocamlfind
+    ocamllsp
+
+Typically you build the DevTools target to ensure all the above binaries are available.
+
 Examples
 ========
 
@@ -19,7 +30,7 @@ Visual Studio Code
 1. Build the DevTools target first (or at least the ocamllsp-Build target).
 2. You have two choices to select the *sandbox* (View > Command Palette, and then
    "OCaml: Select a Sandbox for this Workspace").
-   
+
    Either:
    1. Use "Global OCaml". Prerequisite: You will need to have launched Visual
       Studio Code with `./dk dksdk.cmd.exec CMD code .`
@@ -116,17 +127,29 @@ function(run)
     # Find the program asked for
     find_program(PROG_EXE NAMES ${prog} HINTS ${hints})
 
-    # For ocamllsp in particular, it needs to know where dune is ...
-    # | Internal error: Uncaught exception.
-    # | jsonrpc response error { "code": -32603, "message": "dune binary not found" }
-    # | Raised at Jsonrpc.Response.Error.raise in file "_dn/_opam/.opam-switch/build/ocaml-lsp-server.1.15.1-4.14/jsonrpc/src/jsonrpc.ml", line 188, characters 18-29
-    # | Called from Ocaml_lsp_server__Merlin_config.get_process in file "_dn/_opam/.opam-switch/build/ocaml-lsp-server.1.15.1-4.14/ocaml-lsp-server/src/m", line 267, characters 19-37
-    # | Called from Ocaml_lsp_server__Merlin_config.config in file "_dn/_opam/.opam-switch/build/ocaml-lsp-server.1.15.1-4.14/ocaml-lsp-server/src/m", line 410, characters 17-54
-    # | Called from Fiber__Scheduler.exec in file "_dn/fiber/src/fiber/scheduler.ml", line 73, characters 8-11
-    # \-----------------------------------------------------------------------
     set(cmdPrefix)
-    cmake_path(GET BUILD_CMAKE_DUNE PARENT_PATH duneBinDir)
-    list(APPEND cmdPrefix ${CMAKE_COMMAND} -E env --modify "PATH=path_list_prepend:${duneBinDir}")
+
+    # For ocamllsp in particular ...
+    if(prog STREQUAL ocamllsp)
+        # It needs to know where dune is ...
+        # | Internal error: Uncaught exception.
+        # | jsonrpc response error { "code": -32603, "message": "dune binary not found" }
+        # | Raised at Jsonrpc.Response.Error.raise in file "_dn/_opam/.opam-switch/build/ocaml-lsp-server.1.15.1-4.14/jsonrpc/src/jsonrpc.ml", line 188, characters 18-29
+        # | Called from Ocaml_lsp_server__Merlin_config.get_process in file "_dn/_opam/.opam-switch/build/ocaml-lsp-server.1.15.1-4.14/ocaml-lsp-server/src/m", line 267, characters 19-37
+        # | Called from Ocaml_lsp_server__Merlin_config.config in file "_dn/_opam/.opam-switch/build/ocaml-lsp-server.1.15.1-4.14/ocaml-lsp-server/src/m", line 410, characters 17-54
+        # | Called from Fiber__Scheduler.exec in file "_dn/fiber/src/fiber/scheduler.ml", line 73, characters 8-11
+        # \-----------------------------------------------------------------------
+        cmake_path(GET BUILD_CMAKE_DUNE PARENT_PATH duneBinDir)
+        list(APPEND cmdPrefix ${CMAKE_COMMAND} -E env --modify "PATH=path_list_prepend:${duneBinDir}")
+
+        # It needs to know where ocamlformat is ...
+        # | [Error - 8:45:40 AM] Request textDocument/formatting failed.
+        # |   Message: Unable to find ocamlformat binary. You need to install ocamlformat manually to use the formatting feature.
+        # |  Code: -32600
+        find_program(OCAMLFORMAT_EXE NAMES ocamlformat HINTS ${hints})
+        cmake_path(GET OCAMLFORMAT_EXE PARENT_PATH ocamlformatBinDir)
+        list(APPEND cmdPrefix ${CMAKE_COMMAND} -E env --modify "PATH=path_list_prepend:${ocamlformatBinDir}")
+    endif()
 
     if(EXISTS "${PROG_EXE}.cmd")
         # Windows batch scripts need to go through CMD.EXE /C
