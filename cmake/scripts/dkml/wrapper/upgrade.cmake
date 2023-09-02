077339d6421dbec40f129f56ec5d4bc2cc4d618b
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-09-02 02:28:53 +0000

    Add dkml.wrapper.upgrade HERE and DONE

--- cmake/scripts/dkml/wrapper/upgrade.cmake
+++ cmake/scripts/dkml/wrapper/upgrade.cmake
@@ -25,6 +25,20 @@ Arguments
 
 HELP
   Print this help message.
+
+HERE
+  Do the upgrade in the current directory rather than the
+  directory that ./dk lives.
+  This is useful as the first step in adding ./dk to a
+  new project:
+    git clone https://gitlab.com/diskuv/dktool.git
+    dktool/dk user.dkml.wrapper.upgrade HERE
+
+DONE
+  Remove the dktool/ created by a prior invocation of:
+    git clone https://gitlab.com/diskuv/dktool.git
+  This is useful as the final step in adding ./dk to a
+  new project.
 ]])
 endfunction()
 
@@ -34,10 +48,16 @@ function(run)
 
     set(CMAKE_CURRENT_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CURRENT_FUNCTION})
 
-    cmake_parse_arguments(PARSE_ARGV 0 ARG "HELP" "" "")
+    cmake_parse_arguments(PARSE_ARGV 0 ARG "HELP;HERE;DONE" "" "")
 
     if(ARG_HELP)
       help(MODE NOTICE)
+      return()
+    endif()
+
+    if(ARG_HERE AND ARG_DONE)
+      help(MODE NOTICE)
+      message(FATAL_ERROR "You cannot use both HERE and DONE arguments")
       return()
     endif()
 
@@ -61,14 +81,49 @@ function(run)
       message(FATAL_ERROR "Missing 'FindDkToolScripts.cmake' at expected ${file_cmake_finddktoolsscriptscmake}")
     endif()
 
+    # DONE?
+    if(ARG_DONE)
+      # we already checked that no [HERE] argument
+      if(IS_DIRECTORY "${CMAKE_SOURCE_DIR}/dktool" AND IS_DIRECTORY "${CMAKE_SOURCE_DIR}/dktool/.git")
+        file(REMOVE_RECURSE "${CMAKE_SOURCE_DIR}/dktool")
+      endif()
+      message(NOTICE [[
+Congratulations. Let's get building!
+  Announcements | https://twitter.com/diskuv
+  DkML          | https://diskuv.com/dkmlbook/
+  DkSDK         | https://diskuv.com/pricing/
+  Second OCaml  | https://www.youtube.com/@diskuv/
+]])
+      return()
+    endif()
+
+    # destination
+    set(dest "${CMAKE_SOURCE_DIR}")
+    if(ARG_HERE)
+      set(dest "${DKTOOL_PWD}")
+    endif()
+
     # install
     file(INSTALL "${file_dkcmd}"
-        DESTINATION ${CMAKE_SOURCE_DIR}
+        DESTINATION "${dest}"
         FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ)
     file(INSTALL "${file_cmake_finddktoolsscriptscmake}"
-        DESTINATION ${CMAKE_SOURCE_DIR}/cmake
+        DESTINATION "${dest}/cmake"
         FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ)
     file(INSTALL "${file_dk}"
-        DESTINATION ${CMAKE_SOURCE_DIR}
+        DESTINATION "${dest}"
         FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+
+    # Prompt next steps for HERE users
+    if(ARG_HERE)
+      set(invocation ./dk)
+      if(WIN32)
+        set(invocation [[.\dk]])
+      endif()
+      message(NOTICE "
+The final installation step is to run:
+
+  ${invocation} dkml.wrapper.upgrade DONE
+")
+    endif()
 endfunction()
