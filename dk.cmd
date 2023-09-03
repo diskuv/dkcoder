8ad71bff4b0bba94ea5bfac22d1438bcf7cf23da
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-09-03 14:45:54 -0700

    Relicense a few files with Apache v2
    Only files regarding dkml-workflows.

--- dk.cmd
+++ dk.cmd
@@ -5,11 +5,17 @@ REM # File: dktool/dk.cmd                                                    #
 REM #                                                                        #
 REM # Copyright 2023 Diskuv, Inc.                                            #
 REM #                                                                        #
-REM # Licensed under the Open Software License version 3.0                   #
-REM # (the "License"); you may not use this file except in compliance        #
-REM # with the License. You may obtain a copy of the License at              #
+REM # Licensed under the Apache License, Version 2.0 (the "License");        #
+REM # you may not use this file except in compliance with the License.       #
+REM # You may obtain a copy of the License at                                #
 REM #                                                                        #
-REM #     https://opensource.org/license/osl-3-0-php/                        #
+REM #     http://www.apache.org/licenses/LICENSE-2.0                         #
+REM #                                                                        #
+REM # Unless required by applicable law or agreed to in writing, software    #
+REM # distributed under the License is distributed on an "AS IS" BASIS,      #
+REM # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        #
+REM # implied. See the License for the specific language governing           #
+REM # permissions and limitations under the License.                         #
 REM #                                                                        #
 REM ##########################################################################
 
