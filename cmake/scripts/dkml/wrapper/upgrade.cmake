5d7d7ce9053d19423183403bf835c26d477ce76c
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-10-04 10:22:12 -0700

    bugfix: Rename + move FindDkToolScripts

--- cmake/scripts/dkml/wrapper/upgrade.cmake
+++ cmake/scripts/dkml/wrapper/upgrade.cmake
@@ -112,7 +112,7 @@ Congratulations. Let's get building!
         DESTINATION "${dest}"
         FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ)
     file(INSTALL "${file_dkfindscriptscmake}"
-        DESTINATION "${dest}/cmake"
+        DESTINATION "${dest}/cmake/scripts"
         FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ)
     file(INSTALL "${file_dk}"
         DESTINATION "${dest}"
