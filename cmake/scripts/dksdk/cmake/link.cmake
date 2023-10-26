591d78db326346696ae9d8e771b38074a87ea2fe
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-10-26 16:29:51 -0700

    Add EXE to dksdk.cmake.link

--- cmake/scripts/dksdk/cmake/link.cmake
+++ cmake/scripts/dksdk/cmake/link.cmake
@@ -30,8 +30,11 @@ Directory Structure
 .ci/cmake/
 ├── bin
 │   ├── cmake -> .../cmake-MAJOR.MINOR.PATCH/bin/cmake
+│   ├── cmake.exe -> cmake
 │   ├── cpack -> .../cmake-MAJOR.MINOR.PATCH/bin/cpack
+│   ├── cpack.exe -> cpack
 │   └── ctest -> .../cmake-MAJOR.MINOR.PATCH/bin/ctest
+│   └── ctest.exe -> ctest
 └── share
     └── cmake-MAJOR.MINOR -> .../cmake-MAJOR.MINOR.PATCH/share/cmake-MAJOR.MINOR/
         ├── include
@@ -39,7 +42,12 @@ Directory Structure
         └── Templates
 
 On Windows the files will be named cmake.exe, cpack.exe,
-and ctest.exe in the ./ci/cmake/bin/ directory.
+and ctest.exe in the ./ci/cmake/bin/ directory, and no
+cmake, cpack and ctest files will exist.
+
+On Unix only the cmake, cpack and ctest files will exist,
+unless the EXE flag is used. With the EXE flag the cmake.exe,
+cpack.exe, and ctest.exe symlinks will be created.
 
 The share/cmake-MAJOR.MINOR directory (the CMAKE_ROOT) may
 have contents different from what you see above. The entire
@@ -50,6 +58,14 @@ Arguments
 
 HELP
   Print this help message.
+
+EXE
+  Create cmake.exe, cpack.exe and ctest.exe symlinks to the
+  cmake, cpack and ctest binaries on Unix. Does nothing on
+  Windows. This flag exists for the use of tools like Visual Studio
+  Code extension CMake Tools that must use hardcoded paths
+  that can't vary across operating systems, especially
+  when saved in shareable files like .vscode/settings.json.
 
 QUIET
   Do not print what files are being installed.
@@ -62,7 +78,7 @@ function(run)
 
     set(CMAKE_CURRENT_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CURRENT_FUNCTION})
 
-    cmake_parse_arguments(PARSE_ARGV 0 ARG "HELP;QUIET" "" "")
+    cmake_parse_arguments(PARSE_ARGV 0 ARG "HELP;QUIET;EXE" "" "")
 
     if(ARG_HELP)
       help(MODE NOTICE)
@@ -85,10 +101,17 @@ function(run)
 
     # bin/
     set(ENV{CMAKE_INSTALL_MODE} ABS_SYMLINK_OR_COPY)
+    set(dest_BIN ${CMAKE_SOURCE_DIR}/.ci/cmake/bin)
     file(${file_COMMAND} ${CMAKE_COMMAND} ${CMAKE_CTEST_COMMAND} ${CMAKE_CPACK_COMMAND}
-        DESTINATION ${CMAKE_SOURCE_DIR}/.ci/cmake/bin
+        DESTINATION ${dest_BIN}
         FOLLOW_SYMLINK_CHAIN
         FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+
+    if(CMAKE_HOST_UNIX AND ARG_EXE)
+        file(CREATE_LINK cmake ${dest_BIN}/cmake.exe COPY_ON_ERROR SYMBOLIC)
+        file(CREATE_LINK cpack ${dest_BIN}/cpack.exe COPY_ON_ERROR SYMBOLIC)
+        file(CREATE_LINK ctest ${dest_BIN}/ctest.exe COPY_ON_ERROR SYMBOLIC)
+    endif()
 
     # <CMAKE_ROOT>/
     file(${file_COMMAND} ${CMAKE_ROOT} DESTINATION ${CMAKE_SOURCE_DIR}/.ci/cmake/share FOLLOW_SYMLINK_CHAIN)
