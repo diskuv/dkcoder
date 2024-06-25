d313d0951d76243496d98766b9bd56d4720195c0
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2024-06-24 21:04:44 -0700

    Add JDK 8 option.
    + No use FOLLOW_SYMLINK_CHAIN which is buggy on macOS

--- cmake/scripts/dksdk/java/jdk/download.cmake
+++ cmake/scripts/dksdk/java/jdk/download.cmake
@@ -52,11 +52,25 @@ HELP
 QUIET
   Do not print CMake STATUS messages.
 
+JDK 17
+  Install JDK 17 (the default).
+
+JDK 8
+  Install JDK 8 to .ci/local/share/jdk8 rather than the default JDK 17.
+
 NO_SYSTEM_PATH
   Do not check for a JDK in well-known locations and in the PATH.
   Instead, install a JDK if no JDK exists at `.ci/local/share/jdk`.
 ")
 endfunction()
+
+macro(set_jdk_folder)
+    if(JDK_VERSION EQUAL 8)
+        set(JDK_FOLDER jdk8)
+    else()
+        set(JDK_FOLDER jdk)
+    endif()
+endmacro()
 
 # This is used by other scripts to find the JAVA_HOME.
 #
@@ -80,6 +94,7 @@ function(get_jdk_home)
     if(ARG_JDK_VERSION)
         set(JDK_VERSION ${ARG_JDK_VERSION})
     endif()
+    set_jdk_folder()
 
     # JAVA_HOME in the environment
     if(DEFINED ENV{JAVA_HOME} AND IS_DIRECTORY "$ENV{JAVA_HOME}")
@@ -159,13 +174,16 @@ endfunction()
 # - JAVA
 function(install_java_jdk)
     set(noValues NO_SYSTEM_PATH)
-    set(singleValues)
+    set(singleValues JDK_VERSION)
     set(multiValues)
     cmake_parse_arguments(PARSE_ARGV 0 ARG "${noValues}" "${singleValues}" "${multiValues}")
 
-    set(hints "${CMAKE_SOURCE_DIR}/.ci/local/share/jdk/bin")
+    set(JDK_VERSION ${ARG_JDK_VERSION})
+    set_jdk_folder()
+
+    set(hints "${CMAKE_SOURCE_DIR}/.ci/local/share/${JDK_FOLDER}/bin")
     if(CMAKE_HOST_APPLE)
-        list(PREPEND hints "${CMAKE_SOURCE_DIR}/.ci/local/share/jdk/Contents/Home/bin")
+        list(PREPEND hints "${CMAKE_SOURCE_DIR}/.ci/local/share/${JDK_FOLDER}/Contents/Home/bin")
     endif()
     set(find_program_INITIAL)
     if(ARG_NO_SYSTEM_PATH)
@@ -194,20 +212,22 @@ function(install_java_jdk)
             if(host_machine_type STREQUAL x86_64)
                 set(url https://aka.ms/download-jdk/microsoft-jdk-17.0.8.1-macOS-x64.tar.gz)
                 set(out_base jdk-17.0.8.1+1)
-                message(${loglevel} "Downloading Temurin JDK from ${url}")
-                file(DOWNLOAD ${url}
-                    ${CMAKE_CURRENT_BINARY_DIR}/java.tar.gz
-                    EXPECTED_HASH SHA256=e67ed748b9ef6d4557da24beefe9d9ec193e9d9f843be5ff6559a275e0d230b6)
+                set(expected_sha256 e67ed748b9ef6d4557da24beefe9d9ec193e9d9f843be5ff6559a275e0d230b6)
             elseif(host_machine_type STREQUAL arm64)
-                set(url https://aka.ms/download-jdk/microsoft-jdk-17.0.8.1-macOS-aarch64.tar.gz)
-                set(out_base jdk-17.0.8.1+1)
-                message(${loglevel} "Downloading Temurin JDK from ${url}")
-                file(DOWNLOAD ${url}
-                    ${CMAKE_CURRENT_BINARY_DIR}/java.tar.gz
-                    EXPECTED_HASH SHA256=8acda4fa59946902180a9283ee191b3db19b8c1146fb8dfa209d316ec78f9a5f)
+                if(JDK_VERSION EQUAL 8)
+                    set(url https://cdn.azul.com/zulu/bin/zulu8.78.0.19-ca-jdk8.0.412-macosx_aarch64.tar.gz)
+                    set(out_base zulu8.78.0.19-ca-jdk8.0.412-macosx_aarch64)
+                    set(expected_sha256 35bc35808379400e4a70e1f7ee379778881799b93c2cc9fe1ae515c03c2fb057)
+                else()
+                    set(url https://aka.ms/download-jdk/microsoft-jdk-17.0.8.1-macOS-aarch64.tar.gz)
+                    set(out_base jdk-17.0.8.1+1)
+                    set(expected_sha256 8acda4fa59946902180a9283ee191b3db19b8c1146fb8dfa209d316ec78f9a5f)
+                endif()
             else()
                 message(FATAL_ERROR "Your APPLE ${host_machine_type} platform is currently not supported by this download script")
             endif()
+            message(${loglevel} "Downloading JDK from ${url}")                
+            file(DOWNLOAD ${url} ${CMAKE_CURRENT_BINARY_DIR}/java.tar.gz EXPECTED_HASH SHA256=${expected_sha256})
             message(${loglevel} "Extracting JDK")
             file(ARCHIVE_EXTRACT INPUT ${CMAKE_CURRENT_BINARY_DIR}/java.tar.gz DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
             set(downloaded ON)
@@ -238,24 +258,24 @@ function(install_java_jdk)
             set(downloaded ON)
         endif()
         if(downloaded)
-            file(REMOVE_RECURSE "${CMAKE_SOURCE_DIR}/.ci/local/share/jdk")
-            file(MAKE_DIRECTORY "${CMAKE_SOURCE_DIR}/.ci/local/share/jdk")
+            file(REMOVE_RECURSE "${CMAKE_SOURCE_DIR}/.ci/local/share/${JDK_FOLDER}")
+            file(MAKE_DIRECTORY "${CMAKE_SOURCE_DIR}/.ci/local/share/${JDK_FOLDER}")
 
             # gitignore
             file(COPY_FILE
                 "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../../__dk-tmpl/all.gitignore"
-                "${CMAKE_SOURCE_DIR}/.ci/local/share/jdk/.gitignore"
+                "${CMAKE_SOURCE_DIR}/.ci/local/share/${JDK_FOLDER}/.gitignore"
                 ONLY_IF_DIFFERENT)
 
             # Do file(RENAME) but work across mount volumes (ex. inside containers)
             file(GLOB entries
                 LIST_DIRECTORIES true
-                RELATIVE ${CMAKE_CURRENT_BINARY_DIR}/${out_base}
+                RELATIVE "${CMAKE_CURRENT_BINARY_DIR}/${out_base}"
                 ${CMAKE_CURRENT_BINARY_DIR}/${out_base}/*)
             foreach(entry IN LISTS entries)
-                file(COPY ${CMAKE_CURRENT_BINARY_DIR}/${out_base}/${entry}
-                    DESTINATION ${CMAKE_SOURCE_DIR}/.ci/local/share/jdk
-                    FOLLOW_SYMLINK_CHAIN
+                file(COPY "${CMAKE_CURRENT_BINARY_DIR}/${out_base}/${entry}"
+                    DESTINATION "${CMAKE_SOURCE_DIR}/.ci/local/share/${JDK_FOLDER}"
+                    # [FOLLOW_SYMLINK_CHAIN] fails on JDK8 macos with bin -> bin
                     USE_SOURCE_PERMISSIONS)
             endforeach()
             file(REMOVE_RECURSE "${CMAKE_CURRENT_BINARY_DIR}/${out_base}")
@@ -272,7 +292,7 @@ function(run)
     # Get helper functions from this file
     include(${CMAKE_CURRENT_FUNCTION_LIST_FILE})
 
-    cmake_parse_arguments(PARSE_ARGV 0 ARG "HELP;QUIET;NO_SYSTEM_PATH" "" "")
+    cmake_parse_arguments(PARSE_ARGV 0 ARG "HELP;QUIET;NO_SYSTEM_PATH" "JDK" "")
 
     if(ARG_HELP)
         help(MODE NOTICE)
@@ -286,12 +306,18 @@ function(run)
         set(loglevel STATUS)
     endif()
 
+    # JDK
+    set(args_JDK JDK_VERSION 17)
+    if(ARG_JDK EQUAL 8)
+        set(args_JDK JDK_VERSION 8)
+    endif()
+
     # NO_SYSTEM_PATH
     set(expand_NO_SYSTEM_PATH)
     if(ARG_NO_SYSTEM_PATH)
         list(APPEND expand_NO_SYSTEM_PATH NO_SYSTEM_PATH)
     endif()
 
-    install_java_jdk(${expand_NO_SYSTEM_PATH})
+    install_java_jdk(${expand_NO_SYSTEM_PATH} ${args_JDK})
     message(STATUS "javac compiler is at: ${JAVAC}")
 endfunction()
