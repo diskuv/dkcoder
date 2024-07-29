8caa678bd63d5971a91f231dc726e851e3244a70
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2024-07-29 05:35:40 -0700

    Prepare 0.4.0-2 (2/2)

--- __dk.cmake
+++ __dk.cmake
@@ -63,8 +63,8 @@ set(__DkRun_V0_4_COMPILE_VERSION 0.4.0-2)
 set(__DkRun_V0_4_URL_BASE https://gitlab.com/api/v4/projects/52918795/packages/generic/stdexport/${__DkRun_V0_4_COMPILE_VERSION})
 set(__DkRun_V0_4_SHA256_linux_x86_64   ec7964fc69963b18eefd9e47e154c69e19464ebac3f3e3df5a9e1e69e29acabe)
 set(__DkRun_V0_4_SHA256_linux_x86      todo_v0_5_release)
-set(__DkRun_V0_4_SHA256_darwin_x86_64  9314bab638e32d00ef45db42ea9f95427bc31cc233b55a373fc23ef10dfd9600)#unsigned
-set(__DkRun_V0_4_SHA256_darwin_arm64   9b412798fd8c9f83e016022e1eb5cbd815e36b2b91a57bc4e17b381617008e07)#unsigned
+set(__DkRun_V0_4_SHA256_darwin_x86_64  d7d0d1d5d4fd289dcf9eb2057869b4c170e97f481a78e63baeb62c03e34a7f39)
+set(__DkRun_V0_4_SHA256_darwin_arm64   7525036f7dcc10b38118925350ddfffeb6185df316ac12822cdd50817288aa06)
 set(__DkRun_V0_4_SHA256_windows_x86_64 394f897d7cfc791f115e5bf427c7f56e054b510cbc96e79bb3ecf8f669b0ecc0)
 set(__DkRun_V0_4_SHA256_windows_x86    d0561ee1b49728b05e008df16146bd6e282199d40fd9e67c21f059eee3aa3469)
 set(__DkRun_V0_4_EOL_YYYY_MM_DD "2025-06-30")
