68d816c1efab4a5394c545ff41f3ba8347850420
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2024-09-23 09:39:02 -0700

    Standalone script for downloading arbitrary Android package

--- cmake/scripts/dksdk/android/ndk/download.cmake
+++ cmake/scripts/dksdk/android/ndk/download.cmake
@@ -23,8 +23,9 @@ function(help)
 
     message(${ARG_MODE} "usage: ./dk dksdk.android.ndk.download
 
-Downloads Android NDK ${NDK_LTS}, Android SDK Platform ${SDK_PLATFORM}
-and if needed a Java JDK as well.
+Downloads versions of 'NDK (Side by side) ${NDK_LTS}' and 'Android SDK Platform ${SDK_PLATFORM}'
+packages supported by DkSDK, and if needed accepts licenses and downloads
+a Java JDK and Android SDK Manager as well.
 
 Only meant for CI use, after you have already accepted the terms
 for Android NDK elsewhere.
@@ -39,8 +40,28 @@ We recommend that you use:
     ./dk dksdk.android.cmake.download
 to download an Android CMake version containing Ninja.
 
-The Android SDK Platform is downloaded so Android Studio can recognize
-the directory structure below.
+Packages
+========
+
+The following packages are downloaded with versions supported by DkSDK:
+
+  Package                          | Description
+  -------                          | -------
+  ndk;${NDK_LTS}                          | NDK (Side by side)
+  platforms;android-${SDK_PLATFORM}            | Android SDK Platform
+
+The Android SDK Platform (ex. adb.exe) is downloaded so Android Studio can
+recognize the directory structure (described below) as a valid choice of
+'Android SDK Location'.
+
+However, to use Android Studio's Device Manager, you will need other
+packages using `./dk dksdk.android.pkg.download`:
+
+  Package                                     | Description
+  -------                                     | -------
+  emulator                                    | Android Emulator
+  platform-tools                              | Android SDK Platform-Tools
+  system-images;android-31;google_apis;x86_64 | Google APIs Intel x86_64 Atom System Image
 
 Directory Structure
 ===================
@@ -96,18 +117,6 @@ Places the NDK in .ci/local/share/android-sdk:
             ├── source.properties
             ├── templates
             └── uiautomator.jar
-    
-Proxies
-=======
-
-The Android SDK Manager, which is used to download the Android NDK,
-supports HTTP proxies. If your environment must use an HTTP proxy to
-download from the Internet, you can set the environment variable 'http_proxy'
-to the URL of your HTTP proxy.
-Examples: http://proxy_host:3182 or http://proxy_host:8080.
-Authenticated proxies with a username and password are not supported.
-Set the environment variable 'https_proxy' if you have a
-https://proxy_host:proxy_port proxy.
 
 Arguments
 =========
@@ -124,139 +133,11 @@ NO_SYSTEM_PATH
 ")
 endfunction()
 
-set(sdkmanager_NAMES sdkmanager sdkmanager.bat)
-
-# Sets SDKMANAGER and SDKMANAGER_COMMON_ARGS.
-# SDKMANAGER_COMMON_ARGS is used for no-authentication proxy settings.
-function(find_sdkmanager)
-    set(noValues REQUIRED NO_SYSTEM_PATH)
-    set(singleValues)
-    set(multiValues)
-    cmake_parse_arguments(PARSE_ARGV 0 ARG "${noValues}" "${singleValues}" "${multiValues}")
-
-    set(find_ARGS)
-    if(ARG_NO_SYSTEM_PATH)
-        list(APPEND find_ARGS NO_DEFAULT_PATH)
-    endif()
-    if(ARG_REQUIRED)
-        list(APPEND find_ARGS REQUIRED)
-    endif()
-
-    set(hints ${CMAKE_SOURCE_DIR}/.ci/local/share/android-sdk/cmdline-tools/latest/bin)
-    find_program(SDKMANAGER NAMES ${sdkmanager_NAMES} HINTS ${hints} ${find_ARGS})
-
-    # Any HTTP proxy? We follow the curl standards at https://everything.curl.dev/usingcurl/proxies/env
-    # which do not allow HTTP_PROXY.
-    set(proxy_ARGS)
-    if(DEFINED ENV{http_proxy})
-        set(url $ENV{http_proxy})
-        if(url MATCHES [[^http://([^:]+):([0-9]+).*]])
-            list(APPEND proxy_ARGS --no_https --proxy=http "--proxy_host=${CMAKE_MATCH_1}" --proxy_port=${CMAKE_MATCH_2})
-        endif()
-    elseif(DEFINED ENV{https_proxy} OR DEFINED ENV{HTTPS_PROXY})
-        if(DEFINED ENV{https_proxy})
-            set(url $ENV{https_proxy})
-        else()
-            set(url $ENV{HTTPS_PROXY})
-        endif()
-        if(url MATCHES [[^https://([^:]+):([0-9]+).*]])
-            list(APPEND proxy_ARGS --proxy=http "--proxy_host=${CMAKE_MATCH_1}" --proxy_port=${CMAKE_MATCH_2})
-        endif()
-    endif()
-
-    set(SDKMANAGER_COMMON_ARGS "${proxy_ARGS}" PARENT_SCOPE)
-endfunction()
-
-function(install_sdkmanager)
-    set(noValues NO_SYSTEM_PATH)
-    set(singleValues)
-    set(multiValues)
-    cmake_parse_arguments(PARSE_ARGV 0 ARG "${noValues}" "${singleValues}" "${multiValues}")
-
-    # NO_SYSTEM_PATH
-    set(expand_NO_SYSTEM_PATH)
-    if(ARG_NO_SYSTEM_PATH)
-        list(APPEND expand_NO_SYSTEM_PATH NO_SYSTEM_PATH)
-    endif()
-
-    find_sdkmanager(${expand_NO_SYSTEM_PATH})
-
-    if(NOT SDKMANAGER)
-        # Download into .ci/local/share/android-sdk/cmdline-tools/latest/bin (which is one of the HINTS)
-        if(CMAKE_HOST_WIN32)
-            set(url https://dl.google.com/android/repository/commandlinetools-win-9477386_latest.zip)
-            message(${loglevel} "Downloading Android NDK from ${url}")
-            file(DOWNLOAD ${url}
-                ${CMAKE_CURRENT_BINARY_DIR}/commandlinetools.zip
-                EXPECTED_HASH SHA256=696431978daadd33a28841320659835ba8db8080a535b8f35e9e60701ab8b491)
-        elseif(CMAKE_HOST_UNIX)
-            set(url https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip)
-            message(${loglevel} "Downloading Android NDK from ${url}")
-            file(DOWNLOAD ${url}
-                ${CMAKE_CURRENT_BINARY_DIR}/commandlinetools.zip
-                EXPECTED_HASH SHA256=bd1aa17c7ef10066949c88dc6c9c8d536be27f992a1f3b5a584f9bd2ba5646a0)
-        else()
-            message(FATAL_ERROR "Your platform is currently not supported by this download script")
-        endif()
-
-        message(${loglevel} "Extracting Android NDK")
-        file(ARCHIVE_EXTRACT INPUT ${CMAKE_CURRENT_BINARY_DIR}/commandlinetools.zip
-            DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
-        file(REMOVE_RECURSE ${CMAKE_SOURCE_DIR}/.ci/local/share/android-sdk/cmdline-tools)
-        file(MAKE_DIRECTORY ${CMAKE_SOURCE_DIR}/.ci/local/share/android-sdk/cmdline-tools)
-        # Do file(RENAME) but work across mount volumes (ex. inside containers)
-        file(GLOB entries
-            LIST_DIRECTORIES true
-            RELATIVE ${CMAKE_CURRENT_BINARY_DIR}/cmdline-tools
-            ${CMAKE_CURRENT_BINARY_DIR}/cmdline-tools/*)
-        foreach(entry IN LISTS entries)
-            file(COPY ${CMAKE_CURRENT_BINARY_DIR}/cmdline-tools/${entry}
-                DESTINATION ${CMAKE_SOURCE_DIR}/.ci/local/share/android-sdk/cmdline-tools/latest
-                FOLLOW_SYMLINK_CHAIN
-                USE_SOURCE_PERMISSIONS)
-        endforeach()
-        file(REMOVE_RECURSE "${CMAKE_CURRENT_BINARY_DIR}/cmdline-tools")
-    endif()
-
-    find_sdkmanager(${expand_NO_SYSTEM_PATH} REQUIRED)
-endfunction()
-
-function(are_licenses_accepted LICENSEDIR)
-    set(licenses android-googletv-license android-sdk-arm-dbt-license android-sdk-license android-sdk-preview-license google-gdk-license mips-android-sysimage-license)
-
-    set(accepted OFF PARENT_SCOPE)
-
-    foreach(license IN LISTS licenses)
-        if(NOT EXISTS "${LICENSEDIR}/${license}")
-            return()
-        endif()
-    endforeach()
-
-    set(accepted ON PARENT_SCOPE)
-endfunction()
-
-function(accept_licenses)
-    are_licenses_accepted(${CMAKE_SOURCE_DIR}/.ci/local/share/android-sdk/licenses)
-
-    if(NOT accepted)
-        string(REPEAT "Y\n" 20 many_yes)
-        file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/yes-licenses" "${many_yes}")
-        execute_process(
-            COMMAND ${run_sdkmanager} --licenses ${SDKMANAGER_COMMON_ARGS}
-            INPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/yes-licenses
-            COMMAND_ERROR_IS_FATAL ANY)
-    endif()
-endfunction()
-
-function(install_sdk)
+function(install_sdk_platform)
     set(ANDROID_JAR ${CMAKE_SOURCE_DIR}/.ci/local/share/android-sdk/platforms/android-${SDK_PLATFORM}/android.jar)
 
     if(NOT EXISTS ${ANDROID_JAR})
-        # Install into .ci/local/share/android-sdk ...
-        message(${loglevel} "Installing Android SDK Platform")
-        execute_process(
-            COMMAND ${run_sdkmanager} --install ${SDKMANAGER_COMMON_ARGS} "platforms;android-${SDK_PLATFORM}"
-            COMMAND_ERROR_IS_FATAL ANY)
+        install_pkg(NAME "Android SDK Platform" PACKAGE "platforms;android-${SDK_PLATFORM}")
     endif()
 
     set(ANDROID_JAR "${ANDROID_JAR}" PARENT_SCOPE)
@@ -266,11 +147,7 @@ function(install_ndk)
     set(ANDROID_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/.ci/local/share/android-sdk/ndk/${NDK_LTS}/build/cmake/android.toolchain.cmake)
 
     if(NOT EXISTS ${ANDROID_TOOLCHAIN_FILE})
-        # Install into .ci/local/share/android-sdk ...
-        message(${loglevel} "Installing Android NDK")
-        execute_process(
-            COMMAND ${run_sdkmanager} --install ${SDKMANAGER_COMMON_ARGS} "ndk;${NDK_LTS}"
-            COMMAND_ERROR_IS_FATAL ANY)
+        install_pkg(NAME "NDK (Side by side)" PACKAGE "ndk;${NDK_LTS}")
     endif()
 
     set(ANDROID_TOOLCHAIN_FILE "${ANDROID_TOOLCHAIN_FILE}" PARENT_SCOPE)
@@ -300,8 +177,11 @@ function(run)
         list(APPEND expand_NO_SYSTEM_PATH NO_SYSTEM_PATH)
     endif()
 
-    # Get helper functions from JDK downlader
+    # Get helper functions (install_java_jdk, get_jdk_home) from JDK downlader
     include(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../java/jdk/download.cmake)
+
+    # Get helper functions (install_sdkmanager, accept_licenses) from Android pkg downlader
+    include(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../pkg/download.cmake)
 
     # gitignore
     file(MAKE_DIRECTORY "${CMAKE_SOURCE_DIR}/.ci/local/share/android-sdk")
@@ -314,9 +194,8 @@ function(run)
     get_jdk_home() # Set JAVA_HOME if available
     install_sdkmanager(${expand_NO_SYSTEM_PATH})
 
-    set(run_sdkmanager ${CMAKE_COMMAND} -E env JAVA_HOME=${JAVA_HOME} ${SDKMANAGER})
-    accept_licenses()
-    install_sdk()
+    accept_google_licenses()
+    install_sdk_platform()
     install_ndk()
     message(${loglevel} "Android SDK Platform JAR is at: ${ANDROID_JAR}")
     message(${loglevel} "Android toolchain file is at: ${ANDROID_TOOLCHAIN_FILE}")
